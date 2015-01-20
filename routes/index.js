9ac9d5759047c37f7bf8c768c739f8bc2c9d683e
asem63 <asem63@gmail.com>
2015-01-20 16:41:37 +0200

    more header changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { sampleText: 'Werks' });
+  res.render('index', { sampleText: 'Werks', logged:true });
 });
 
 module.exports = router;
