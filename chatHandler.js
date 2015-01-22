587141f5cf665dbbf4bafb6b182b5acc9fec5022
asem63 <asem63@gmail.com>
2015-01-23 00:20:23 +0200

    message history in room (server side)

--- chatHandler.js
+++ chatHandler.js
@@ -7,13 +7,13 @@ module.exports = function(io){
 
     io.on('connection', function(socket){
 
-        console.log("URL:"+socket.request.user.userName);
-
+        /* handle user disconnection from room*/
         socket.on('room_disconnect', function(msg){
             console.log("<"+socket.request.user.userName+">: discon from room: " + msg);
             io.sockets.in(msg).emit('user_disconnected', socket.request.user);
         });
 
+        /* handle user disconnection from room*/
         socket.on('chat_message', function(msg){
             var message = "<"+socket.request.user.userName+">: "+msg.mes;
 
@@ -21,6 +21,21 @@ module.exports = function(io){
             db.saveMessage(msg.mes, msg.room);
         });
 
+        /* handle request for all message history in room */
+        socket.on('get_all_messages', function(msg){
+            db.getAllMessages(msg.room, function(err, messages){
+                io.sockets.in(msg.room).emit('get_messages', messages);
+            });
+        });
+
+        /* handle request for 100 last messages from given point in room */
+        socket.on('get_100_messages', function(msg){
+            db.getMessageRange(msg.room, msg.mi - 100, msg.mi, function(err, messages){
+                io.sockets.in(msg.room).emit('get_messages', messages);
+            });
+        });
+
+        /* handle room initialization request */
         socket.on('connected_to_room', function(msg){
             console.log("connected To ROOM:" + msg);
             var roomName = msg;
@@ -31,6 +46,10 @@ module.exports = function(io){
 
             db.getAllUserNamesInRoom(roomName, function (err, result) {
                 io.to(socket.id).emit("userlist", result);
+            });
+
+            db.getRoomMessageCount(roomName, function (err, count) {
+                io.to(socket.id).emit("set_message_index", count);
             });
 
             socket.join(roomName);
